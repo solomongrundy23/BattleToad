12a1ff2cbf37a70f0b61c747d92d0fc64325ba9d
Анисимов Сергей Юрьевич <s.anisimov@csat.ru>
2019-11-27 15:03:54 +0300

    Update Class Files

---
+++ CSV.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BattleToad.Ext;
+
+namespace BattleToad.CSV
+{
+    public class CSV
+    {
+        /// <summary>
+        /// Создать экземплер
+        /// </summary>
+        /// <param name="split_char"></param>
+        /// <param name="quotes"></param>
+        /// <param name="null_Text"></param>
+        public CSV(char split_char = '\t', char? quotes = null, string null_Text = null)
+        {
+            Quotes = quotes;
+            SplitChar = split_char;
+            Null_Text = null_Text;
+        }
+        /// <summary>
+        /// Знак кавычек, если нет, то null
+        /// </summary>
+        public char? Quotes;
+        /// <summary>
+        /// Разделитель колонок
+        /// </summary>
+        public char SplitChar;
+        public string Null_Text;
+        /// <summary>
+        /// Заголовок CSV
+        /// </summary>
+        public string[] Header;
+        /// <summary>
+        /// Загрузить CSV-файл
+        /// </summary>
+        /// <param name="file_name">имя файла</param>
+        /// <param name="first_line_is_header">первая строка, это заголовок</param>
+        public async Task LoadAsync(string file_name, bool first_line_is_header = false)
+            => await Task.Run(() => Load(file_name, first_line_is_header));
+        /// <summary>
+        /// Сохранить данные CSV в файл
+        /// </summary>
+        /// <param name="file_name">имя файла</param>
+        /// <param name="write_header">записывать ли заголовок, если он не Null</param>
+        public async Task SaveAsync(string file_name, bool write_header = true)
+            => await Task.Run(() => Save(file_name, write_header));
+        /// <summary>
+        /// Загрузить CSV-файл
+        /// </summary>
+        /// <param name="file_name">имя файла</param>
+        /// <param name="first_line_is_header">первая строка, это заголовок</param>
+        public void Load(string file_name, bool first_line_is_header = false)
+        {
+            try
+            {
+                List<string[]> result = new List<string[]>();
+                string[] file_data = File.ReadAllText(file_name).GetLines(false);
+                foreach (string file_string in file_data)
+                {
+                    string[] records = ParseLine(file_string);
+                    if (records != null) result.Add(records);
+                }
+                if (first_line_is_header)
+                {
+                    if (result.Count == 0)
+                    {
+                        throw new Exception($"Не могу найти заголовок");
+                    }
+                    else
+                    {
+                        Header = result[0];
+                        result.RemoveAt(0);
+                    }
+                }
+                Data = result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка загрузки CSV-файла. {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// Сохранить данные CSV в файл
+        /// </summary>
+        /// <param name="file_name">имя файла</param>
+        /// <param name="write_header">записывать ли заголовок, если он не Null</param>
+        public void Save(string file_name, bool write_header = true)
+        {
+            try
+            {
+                List<string> result = new List<string>();
+                if (Header != null && write_header) result.Add(UnTrimmerRecord(Header));
+                foreach (string[] records in Data)
+                {
+                    result.Add(UnTrimmerRecord(records));
+                }
+                File.WriteAllLines(file_name, result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка  CSV-файла. {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// Данные CSV, хранятся как список массивов строк
+        /// </summary>
+        public List<string[]> Data = new List<string[]>();
+        private string[] ParseLine(string line)
+        {
+            return line.Split(SplitChar).Select(x => Trimmer(x)).ToArray();
+        }
+        private string Trimmer(string record)
+        {
+            if (record == Null_Text) return null;
+            return Quotes == null ? record : record.Trim(Quotes.Value);
+        }
+        private string UnTrimmer(string record)
+        {
+            if (record == Null_Text) return Null_Text;
+            return Quotes == null ? record : $"{Quotes}{record}{Quotes}";
+        }
+        private string UnTrimmerRecord(string[] records) => string.Join($"{SplitChar}", records.Select(x => UnTrimmer(x)).ToArray());
+
+        /// <summary>
+        /// Запустить сборку мусора GC.Collect()
+        /// </summary>
+        public void GarbageCollect()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ ConsoleEx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using BattleToad.Ext;
+
+namespace BattleToad.ConsoleEx
+{
+    public static class ConsoleEx
+    {
+        public static void Input(string label, out string data)
+        {
+            Console.Write(label);
+            data = Console.ReadLine();
+        }
+        public static void RepeatRepeat(int repeat, string text = "")
+        {
+            for (int i = 0; i < repeat; i++) Console.WriteLine(text);
+        }
+        public static void Print(string text = "")
+        {
+            Console.WriteLine(text);
+        }
+        public static void PrintColor(string text, ConsoleColor front, ConsoleColor back)
+        {
+            Console.ForegroundColor = front;
+            Console.BackgroundColor = back;
+            Console.WriteLine(text);
+        }
+        public static void PrintAtPoint(string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(text);
+        }
+        /// <summary>
+        /// Текстовый прогрессбар
+        /// </summary>
+        public class Progress
+        {
+            private int max;
+            private int length = 20;
+            private delegate string GetProgressTextDelegate(int value);
+            private GetProgressTextDelegate GetProgressText;
+            /// <summary>
+            /// Создать прогрессбар
+            /// </summary>
+            /// <param name="Max"></param>
+            /// <param name="Length"></param>
+            public Progress(int Max, int theme = 0, int Length = 20)
+            {
+                if (length < 10) throw new Exception($"Длина {length} слишком мала, должна быть больше 5");
+                max = Max;
+                length = Length;
+                switch (theme)
+                {
+                    case 0: GetProgressText = Theme1; break;
+                    case 1: GetProgressText = Theme2; break;
+                    case 2: GetProgressText = Theme3; break;
+                    case 3: GetProgressText = Theme4; break;
+                    case 4: GetProgressText = Theme5; break;
+                    case 5: GetProgressText = Theme6; break;
+                    case 6: GetProgressText = Theme7; break;
+                    default: GetProgressText = Theme1; break;
+                }
+            }
+            public string GetProgress(int value) => GetProgressText(value);
+
+            private string Theme1(int value)
+            {
+                if (value > max) value = max;
+                return value == max ? "[OK]" : $"{value * 100 / max}%";
+            }
+            private string Theme2(int value)
+            {
+                if (value > max) value = max;
+                string result = "";
+                int _max = value * (length - 3) / max;
+                return
+                    value ==
+                    max ? $"[OK]".AddToEndWhileLengthNotValid(' ', length)
+                    :
+                    $"|{(result.AddToStartWhileLengthNotValid('=', _max) + '>').AddToEndWhileLengthNotValid(' ', length - 3)}|";
+            }
+            private string Theme3(int value)
+            {
+                if (value > max) value = max;
+                string result = "";
+                int _max = value * (length - 3) / max;
+                return
+                    value ==
+                    max ? $"[OK]".AddToEndWhileLengthNotValid(' ', length)
+                    :
+                    $"[{(result.AddToStartWhileLengthNotValid('=', _max) + '>').AddToEndWhileLengthNotValid('.', length - 3)}]";
+            }
+            private string Theme4(int value)
+            {
+                if (value > max) value = max;
+                string result = "";
+                int _max = value * (length - 2) / max;
+                return
+                    value ==
+                    max ? $"[OK]".AddToEndWhileLengthNotValid(' ', length)
+                    :
+                    $"|{(result.AddToStartWhileLengthNotValid('#', _max)).AddToEndWhileLengthNotValid(' ', length - 3)}|";
+            }
+            private string Theme5(int value)
+            {
+                if (value > max) value = max;
+                string result = "";
+                int _max = value * (length - 2) / max;
+                return
+                    value ==
+                    max ? $"[OK]".AddToEndWhileLengthNotValid(' ', length)
+                    :
+                    $"[{(result.AddToStartWhileLengthNotValid('#', _max)).AddToEndWhileLengthNotValid('.', length - 3)}]";
+            }
+            private string Theme6(int value)
+            {
+                if (value > max) value = max;
+                return value == max ? "[OK]".AddToEndWhileLengthNotValid(' ', max.ToString().Length * 2 + 1) : $"{value}/{max}";
+            }
+            private string Theme7(int value)
+            {
+                if (value > max) value = max;
+                string result = "";
+                int _max = value * (length - 3) / max;
+                return
+                    value ==
+                    max ? $"[OK]".AddToEndWhileLengthNotValid(' ', length)
+                    :
+                    $"[{(result.AddToStartWhileLengthNotValid('=', _max) + '|').AddToEndWhileLengthNotValid('-', length - 3)}]";
+            }
+
+            /// <summary>
+            /// Вывести прогрессбар в косноль
+            /// </summary>
+            /// <param name="value">значение</param>
+            /// <param name="x">позиция x</param>
+            /// <param name="y">позиция y</param>
+            public void GetProgressToConsole(int value, int x, int y)
+                => ConsoleEx.PrintAtPoint(GetProgress(value), x, y);
+        }
+    }
+}
