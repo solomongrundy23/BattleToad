21342c9858b4a1fbe83a4055a41b82915cc40582
Анисимов Сергей Юрьевич <s.anisimov@csat.ru>
2019-12-09 13:39:04 +0300

    PhoneRanger

---
+++ Korrector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Timers;
+
+class Korrector: IDisposable
+{
+    private readonly Timer timer = new Timer();
+    public int TypeOfAutoClearing = 0;
+    public double MaxGarbage = 0;
+    public static class Type
+    {
+        public static int Lite = 0;
+        public static int Normal = 1;
+        public static int Strong = 2;
+    }
+    public bool Enabled
+    {
+        get => timer.Enabled;
+        set => timer.Enabled = value;
+    }
+
+    public Korrector(double interval_ms, double max_garbage = 0)
+    {
+        MaxGarbage = max_garbage;
+        timer.Elapsed += TimerTick;
+        timer.Interval = interval_ms;
+        timer.Start();
+    }
+
+    public void Clearing(int type)
+    {
+        GC.Collect(type);
+    }
+
+    private void TimerTick(object sender, EventArgs args)
+    {
+        if (MaxGarbage == 0)
+            GC.Collect(TypeOfAutoClearing);
+        else
+        {
+            if (MaxGarbage < GC.GetTotalMemory(true))
+            {
+                GC.Collect(TypeOfAutoClearing);
+            }
+        }
+    }
+
+    private bool disposedValue = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                timer.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+
+    ~Korrector()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ PhoneRanger.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BattleToad.Ext;
+
+namespace BattleToad.PhoneRange
+{
+    /// <summary>
+    /// Класс для диапозона телефонных номеров, храняться в ulong
+    /// </summary>
+    public class Range
+    {
+        /// <summary>
+        /// Создать экземпляр класса диапозона телефонных номеров
+        /// </summary>
+        /// <param name="min">Минимальный номер</param>
+        /// <param name="max">Максимальный номер</param>
+        public Range(ulong min, ulong max)
+        {
+            Max = max;
+            Min = min;
+        }
+        /// <summary>
+        /// Минимальный номер
+        /// </summary>
+        public ulong Min;
+        /// <summary>
+        /// Максимальный номер
+        /// </summary>
+        public ulong Max;
+        /// <summary>
+        /// Вывести диапозон в строку
+        /// </summary>
+        public string String => Min == Max ? Min.ToString() : $"{Min}\t{Max}";
+    }
+
+    public class PhoneRanger : IDisposable
+    {
+        /// <summary>
+        /// Запуск GarbageCollector
+        /// </summary>
+        public void Garbage() => GC.Collect(GC.MaxGeneration);
+        /// <summary>
+        /// Список диапозонов, при добавлении диапозонов вручную нужно обеспечивать праверку правильности заполнения диапозонами номера
+        /// </summary>
+        public List<Range> Ranges = new List<Range>();
+        /// <summary>
+        /// Получить список диапозонов из текста, содержащего телефонные номера
+        /// </summary>
+        /// <param name="text">текст</param>
+        public void Parse(string text) => Parse(text.GetWords());
+        /// <summary>
+        /// Получить список диапозонов из массива строк, содержащих по одному телефонному номеру
+        /// </summary>
+        /// <param name="records">массив строк, содержащих по одному телефонному номеру</param>
+        public void Parse(string[] records)
+        {
+            try
+            {
+                Ranges.Clear();
+                var nums = records
+                    .Select(x => RusPhoneNumbers.GetPhoneNumber(x, "7"))
+                    .Where(x => x != null)
+                    .Select(x => ulong.Parse(x))
+                    .Distinct()
+                    .OrderBy(x => x)
+                    .ToArray();
+                if (nums.Length == 0) return;
+                var range = new Range(nums[0], nums[0]);
+                for (int i = 1; i < nums.Count(); i++)
+                {
+                    if (range.Max + 1 == nums[i])
+                    {
+                        range.Max = nums[i];
+                        if (i + 1 == nums.Count()) Ranges.Add(range);
+                    }
+                    else
+                    {
+                        Ranges.Add(range);
+                        range = new Range(nums[i], nums[i]);
+                    }
+                }
+            }
+            catch
+            {
+                throw new Exception("Не удалось произвести парсинг данных, проверьте вводимые данные");
+            }
+            finally
+            {
+                Garbage();
+            }
+        }
+        /// <summary>
+        /// Разбить диапозоны на список
+        /// </summary>
+        /// <param name="Merge">Объединение диапозонов</param>
+        /// <returns></returns>
+        public List<string> BreakingRange(bool Merge = false)
+        {
+            try
+            {
+                var result = new List<string>();
+                foreach (var range in Ranges)
+                {
+                    if (range != null)
+                        if (Merge)
+                            result.AddRange(DissolutionCompress(range));
+                        else
+                            result.AddRange(Dissolution(range));
+                }
+                return result;
+            }
+            finally
+            {
+                Garbage();
+            }
+        }
+
+        private string Cut(Range range)
+        {
+            try
+            {
+                string sa = range.Min.ToString();
+                string sb = range.Max.ToString();
+                if (sa == sb) return sa;
+                while (sa.Last() == '0' && sb.Last() == '9')
+                {
+                    sa = sa.Substring(0, sa.Length - 1);
+                    sb = sb.Substring(0, sb.Length - 1);
+                }
+                if (sa == sb) return sa.AddToEndWhileLengthNotValid('.', 11);
+                return null;
+            }
+            catch
+            {
+                throw new Exception("Не верные данные");
+            }
+        }
+
+        private List<string> Dissolution(Range range)
+        {
+            var result = new List<string>();
+
+            string Cutted = Cut(range);
+            if (Cutted != null)
+            {
+                result.Add($"^{Cutted}$");
+            }
+            else
+            {
+                result.AddRange(
+                    ToMasks(range.Min, range.Max)
+                    .Select(x => x.ToString()).ToArray());
+            }
+            return result;
+        }
+
+        private List<string> DissolutionCompress(Range range)
+        {
+            var result = new List<string>();
+            string Cutted = Cut(range);
+            if (Cutted != null)
+            {
+                result.Add($"^{Cutted}$");
+            }
+            else
+            {
+                string[] list =
+                    ToMasks(range.Min, range.Max)
+                    .Select(x => x.ToString()).ToArray();
+                string old = list[0].Substring(0, list[0].Length - 1);
+                int A = int.Parse(list[0].Substring(list[0].Length - 1, 1));
+                int B = 0;
+                int C = A;
+                bool recorded = false;
+                for (int i = 1; i < list.Length; i++)
+                {
+                    if (list[i] != "")
+                    {
+                        B = int.Parse(list[i].Substring(list[i].Length - 1, 1));
+                        if ((C + 1 != B) | (old.Length + 1 != list[i].Length))
+                        {
+                            string temp_s = old + "[" + A + "-" + C + "]";
+                            int j = old.Length;
+                            while (j < 10)
+                            {
+                                temp_s += ".";
+                                j++;
+                            }
+                            result.Add("^" + temp_s + "$");
+                            A = B;
+                            C = B;
+                            old = list[i].Substring(0, list[i].Length - 1);
+                        }
+                        else
+                        {
+                            C = B;
+                            old = list[i].Substring(0, list[i].Length - 1);
+                        }
+                    }
+                }
+                if (!recorded)
+                {
+                    string temp_s = old + "[" + A + "-" + C + "]";
+                    int j = old.Length;
+                    while (j < 10)
+                    {
+                        temp_s += ".";
+                        j++;
+                    }
+                    result.Add("^" + temp_s + "$");
+                }
+                for (int i = 0; i <= 9; i++) result = result.Select(x => x.Replace($"[{i}-{i}]", $"{i}")).ToList();
+                result = result.Select(x => x.Replace("[0-9]", "0")).ToList();
+            }
+            return result;
+        }
+
+        private static IEnumerable<UInt64> ToMasks(UInt64 start, UInt64 end)
+        {
+            while (start <= end)
+            {
+                UInt64 mask = 0, pow = 0;
+                for (int i = 0; ; i++)
+                {
+                    pow = (uint)Math.Pow(10, 7 - i);
+                    mask = start / pow;
+                    if (mask * pow + pow - 1 <= end && mask * pow >= start)
+                    {
+                        yield return mask;
+                        break;
+                    }
+                }
+                start = (mask + 1) * pow;
+            }
+        }
+
+        ~PhoneRanger()
+        {
+           Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (Ranges != null) Ranges = null;
+            Garbage();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
